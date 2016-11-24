07980c6edaae696fc6d35eb06321e448a247529c
Harshit Sharma <hsharma369@gatech.edu>
2016-11-24 01:17:47 -0500

    drop columns

--- SQL_Scripts/Data_Cleaning.sql
+++ SQL_Scripts/Data_Cleaning.sql
@@ -1,1 +1,47 @@
-DELETE FROM _ WHERE 
+ALTER TABLE `infovis`.c2008mccain DROP cmte_id;
+ALTER TABLE `infovis`.c2008mccain DROP cand_nm;
+ALTER TABLE `infovis`.c2008mccain DROP cand_id;
+
+ALTER TABLE `infovis`.c2008obama DROP cmte_id;
+ALTER TABLE `infovis`.c2008obama DROP cand_nm;
+ALTER TABLE `infovis`.c2008obama DROP cand_id;
+
+ALTER TABLE `infovis`.c2012mitt DROP cmte_id;
+ALTER TABLE `infovis`.c2012mitt DROP cand_nm;
+ALTER TABLE `infovis`.c2012mitt DROP cand_id;
+
+ALTER TABLE `infovis`.c2012obama DROP cmte_id;
+ALTER TABLE `infovis`.c2012obama DROP cand_nm;
+ALTER TABLE `infovis`.c2012obama DROP cand_id;
+
+ALTER TABLE `infovis`.c2016clinton DROP cmte_id;
+ALTER TABLE `infovis`.c2016clinton DROP cand_nm;
+ALTER TABLE `infovis`.c2016clinton DROP cand_id;
+
+ALTER TABLE `infovis`.c2016trump DROP cmte_id;
+ALTER TABLE `infovis`.c2016trump DROP cand_nm;
+ALTER TABLE `infovis`.c2016trump DROP cand_id;
+
+ALTER TABLE `infovis`.e2008mccain DROP cmte_id;
+ALTER TABLE `infovis`.e2008mccain DROP cand_nm;
+ALTER TABLE `infovis`.e2008mccain DROP cand_id;
+
+ALTER TABLE `infovis`.e2008obama DROP cmte_id;
+ALTER TABLE `infovis`.e2008obama DROP cand_nm;
+ALTER TABLE `infovis`.e2008obama DROP cand_id;
+
+ALTER TABLE `infovis`.e2012mitt DROP cmte_id;
+ALTER TABLE `infovis`.e2012mitt DROP cand_nm;
+ALTER TABLE `infovis`.e2012mitt DROP cand_id;
+
+ALTER TABLE `infovis`.e2012obama DROP cmte_id;
+ALTER TABLE `infovis`.e2012obama DROP cand_nm;
+ALTER TABLE `infovis`.e2012obama DROP cand_id;
+
+ALTER TABLE `infovis`.e2016clinton DROP cmte_id;
+ALTER TABLE `infovis`.e2016clinton DROP cand_nm;
+ALTER TABLE `infovis`.e2016clinton DROP cand_id;
+
+ALTER TABLE `infovis`.e2016trump DROP cmte_id;
+ALTER TABLE `infovis`.e2016trump DROP cand_nm;
+ALTER TABLE `infovis`.e2016trump DROP cand_id;
